7aedf087ac24b77197cf5c475de5dd5af0b3d996
Nicolas <nalfonso@student.42lisboa.com>
2026-07-22 22:51:18 +0100

    begin of the implemetation of DDA

--- src/render/raycast.c
+++ src/render/raycast.c
@@ -6,22 +6,11 @@
 /*   By: nalfonso <nalfonso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 18:19:57 by nalfonso          #+#    #+#             */
-/*   Updated: 2026/07/21 22:07:30 by nalfonso         ###   ########.fr       */
+/*   Updated: 2026/07/22 22:49:05 by nalfonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cub3d.h"
-
-/* static double camera_dir(t_game *g, double x)
-{
-	// cameraX = 2 * x / win_w - 1
-
-	double	cameraX = 0.0;
-
-	cameraX = (2 * x) / (g->win_w - 1);
-
-	return (cameraX);
-} */
 
 void raycast(t_game *g)
 {
@@ -36,10 +25,48 @@ void raycast(t_game *g)
 	{
 		//cameraX = camera_dir(g, (double)i);
 		cameraX = (2.0 * (double)i) / (double)g->win_w - 1.0;
-		rayDirx = (g->player.dir_x + g->player.plane_x) * cameraX;
-		rayDiry = (g->player.dir_y + g->player.plane_y) * cameraX;
+		rayDirx = g->player.dir_x + g->player.plane_x * cameraX;
+		rayDiry = g->player.dir_y + g->player.plane_y * cameraX;
 		//rayDir = rayDirx / rayDiry; rayDir = %f\n
 		printf("Counter [%i]-| CameraX = %f | rayDirx = %f | rayDiry = %f |\n", i, cameraX , rayDirx, rayDiry);
 		i++;
+	}
+}
+
+
+// DDA variables 
+
+// Formula for Lenght of the Ray 
+// fabs(1/ rayDirX)
+
+double	lenght_ray(t_game *g, double rayDir)
+{
+	double	lenght = 0.0;
+
+	lenght = 1 / rayDir;
+
+	return (lenght);
+}
+
+void checker(t_game *g, double rayDirX, double rayDirY)
+{
+	char	**map;
+	int		y;
+	int		x;
+	double	deltaDisX;
+	double	deltaDISy;
+
+	map = g->map.grid;
+	y = 0;
+	while (y < g->win_h)
+	{
+		x = 0;
+		while (x < g->win_w)
+		{
+			deltaDisX = lenght_ray(g, rayDirX);
+			deltaDISy = lenght_ray(g, rayDirY);
+			x++;
+		}
+		y++;
 	}
 }
